3e2f48c9a29006994941bdd4e440c53eb1357957
Andreas Beyer <abeyer@posteo.de>
2022-04-09 20:43:31 +0200

    add rnPaper and vector icons for ios

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import {Text} from 'react-native';
 import Screen from './src/components/Screen';
+import {Provider as PaperProvider, Button} from 'react-native-paper';
 const App = (): JSX.Element => {
   return (
-    <Screen>
-      <Text>Hello</Text>
-    </Screen>
+    <PaperProvider>
+      <Screen>
+        <Text>Hello</Text>
+        <Button icon="camera">Press me</Button>
+      </Screen>
+    </PaperProvider>
   );
 };
 
