7947624b6e762393f9fcf9cd7d1c8fb9b2cd04a6
Andreas Beyer <abeyer@posteo.de>
2022-04-30 10:47:49 +0200

    rm RN-Paper

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import {Provider as PaperProvider, Button} from 'react-native-paper';
 
 import ThemeProvider from './src/components/ThemeProvider';
 import Navigator from './src/screens/Navigator';
 
 const App = (): JSX.Element => {
-  return (
-    <PaperProvider>
-      <ThemeProvider>
-        <Navigator />
-      </ThemeProvider>
-    </PaperProvider>
-  );
+	return (
+		<ThemeProvider>
+			<Navigator />
+		</ThemeProvider>
+	);
 };
 
 export default App;
